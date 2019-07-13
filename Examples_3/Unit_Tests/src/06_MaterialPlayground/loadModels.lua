558fc91bfbaec43425b73e169bd696a39ca7faee
Dean Calver <me@deanoc.com>
2019-07-13 09:28:58 +0300

    WIP rearranging

--- Examples_3/Unit_Tests/src/06_MaterialPlayground/loadModels.lua
+++
@@ -1,3 +0,0 @@
-loader.LoadModel("matBall.obj") --here model for materials showcase is loaded
-loader.LoadModel("cube.obj") --here model for signs is loaded
-loader.LoadModel("capsule.obj") --here model for signs is loaded
