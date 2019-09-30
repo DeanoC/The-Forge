a8f05c0685c819de08c02b4352af011c098fb221
Deano <me@deanoc.com>
2019-09-30 16:27:15 +0300

    TheForge now uses al2o3 memory allocator al2o3 memory now has an aalloc function for aligned alloc Os now uses same api as other Unixes

--- src/OS/Interfaces/IMemory.h
+++ src/OS/Interfaces/IMemory.h
@@ -31,13 +31,7 @@
 
 #ifndef IMEMORY_H
 #define IMEMORY_H
-#include <new>
-
-void* conf_malloc_internal(size_t size, const char *f, int l, const char *sf);
-void* conf_memalign_internal(size_t align, size_t size, const char *f, int l, const char *sf);
-void* conf_calloc_internal(size_t count, size_t size, const char *f, int l, const char *sf);
-void* conf_realloc_internal(void* ptr, size_t size, const char *f, int l, const char *sf);
-void  conf_free_internal(void* ptr, const char *f, int l, const char *sf);
+#include "al2o3_memory/memory.h"
 
 template <typename T, typename... Args>
 static T* conf_placement_new(void* ptr, Args... args)
@@ -48,7 +42,7 @@ static T* conf_placement_new(void* ptr, Args... args)
 template <typename T, typename... Args>
 static T* conf_new_internal(const char *f, int l, const char *sf, Args... args)
 {
-	T* ptr = (T*)conf_malloc_internal(sizeof(T), f, l, sf);
+	T* ptr = (T*)MEMORY_MALLOC(sizeof(T));
 	return new (ptr) T(args...);
 }
 
@@ -58,31 +52,18 @@ static void conf_delete_internal(T* ptr, const char *f, int l, const char *sf)
 	if (ptr)
 	{
 		ptr->~T();
-		conf_free_internal(ptr, f, l, sf);
+		MEMORY_FREE(ptr); // TODO tracking info
 	}
 }
 
-#ifndef conf_malloc
-#define conf_malloc(size) conf_malloc_internal(size, __FILE__, __LINE__, __FUNCTION__)
-#endif
-#ifndef conf_memalign
-#define conf_memalign(align,size) conf_memalign_internal(align, size, __FILE__, __LINE__, __FUNCTION__)
-#endif
-#ifndef conf_calloc
-#define conf_calloc(count,size) conf_calloc_internal(count, size, __FILE__, __LINE__, __FUNCTION__)
-#endif
-#ifndef conf_realloc
-#define conf_realloc(ptr,size) conf_realloc_internal(ptr, size, __FILE__, __LINE__, __FUNCTION__)
-#endif
-#ifndef conf_free
-#define conf_free(ptr) conf_free_internal(ptr,  __FILE__, __LINE__, __FUNCTION__)
-#endif
-#ifndef conf_new
+#define conf_malloc(size) MEMORY_MALLOC(size)
+#define conf_memalign(align,size) MEMORY_AALLOC(size, align)
+
+#define conf_calloc(count,size) MEMORY_CALLOC(count, size)
+#define conf_realloc(ptr,size) MEMORY_REALLOC(ptr, size)
+#define conf_free(ptr) MEMORY_FREE(ptr)
 #define conf_new(ObjectType, ...) conf_new_internal<ObjectType>(__FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#endif
-#ifndef conf_delete
 #define conf_delete(ptr) conf_delete_internal(ptr,  __FILE__, __LINE__, __FUNCTION__)
-#endif
 
 #endif 
 
