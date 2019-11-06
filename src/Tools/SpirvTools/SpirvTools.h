7fd13fd413c3647fc6eff73803e28a68b28d7409
Deano <me@deanoc.com>
2019-11-06 17:23:58 +0200

    Add some missing tools
    update neon math code
    Added SPIRV_Cross for vulkan shader reflection

--- src/Tools/SpirvTools/SpirvTools.h
+++ src/Tools/SpirvTools/SpirvTools.h
@@ -34,37 +34,37 @@
 
 struct SPIRV_Type
 {
-	// Resources are identified with their SPIR-V ID.
-	// This is the ID of the OpVariable.
-	uint32_t id;
+   // Resources are identified with their SPIR-V ID.
+   // This is the ID of the OpVariable.
+   uint32_t id;
 
-	// The type ID of the variable which includes arrays and all type modifications.
-	// This type ID is not suitable for parsing OpMemberDecoration of a struct and other decorations in general
-	// since these modifications typically happen on the base_type_id.
-	uint32_t type_id;
+   // The type ID of the variable which includes arrays and all type modifications.
+   // This type ID is not suitable for parsing OpMemberDecoration of a struct and other decorations in general
+   // since these modifications typically happen on the base_type_id.
+   uint32_t type_id;
 
-	// The base type of the declared resource.
-	// This type is the base type which ignores pointers and arrays of the type_id.
-	// This is mostly useful to parse decorations of the underlying type.
-	// base_type_id can also be obtained with get_type(get_type(type_id).self).
-	uint32_t base_type_id;
+   // The base type of the declared resource.
+   // This type is the base type which ignores pointers and arrays of the type_id.
+   // This is mostly useful to parse decorations of the underlying type.
+   // base_type_id can also be obtained with get_type(get_type(type_id).self).
+   uint32_t base_type_id;
 };
 
 enum SPIRV_Resource_Type
 {
-	SPIRV_TYPE_STAGE_INPUTS = 0,
-	SPIRV_TYPE_STAGE_OUTPUTS,
-	SPIRV_TYPE_UNIFORM_BUFFERS,
-	SPIRV_TYPE_STORAGE_BUFFERS,
-	SPIRV_TYPE_IMAGES,
-	SPIRV_TYPE_STORAGE_IMAGES,
-	SPIRV_TYPE_SAMPLERS,
-	SPIRV_TYPE_PUSH_CONSTANT,
-	SPIRV_TYPE_SUBPASS_INPUTS,
-	SPIRV_TYPE_UNIFORM_TEXEL_BUFFERS,
-	SPIRV_TYPE_STORAGE_TEXEL_BUFFERS,
-	SPIRV_TYPE_ACCELERATION_STRUCTURES,
-	SPIRV_TYPE_COUNT
+   SPIRV_TYPE_STAGE_INPUTS = 0,
+   SPIRV_TYPE_STAGE_OUTPUTS,
+   SPIRV_TYPE_UNIFORM_BUFFERS,
+   SPIRV_TYPE_STORAGE_BUFFERS,
+   SPIRV_TYPE_IMAGES,
+   SPIRV_TYPE_STORAGE_IMAGES,
+   SPIRV_TYPE_SAMPLERS,
+   SPIRV_TYPE_PUSH_CONSTANT,
+   SPIRV_TYPE_SUBPASS_INPUTS,
+   SPIRV_TYPE_UNIFORM_TEXEL_BUFFERS,
+   SPIRV_TYPE_STORAGE_TEXEL_BUFFERS,
+   SPIRV_TYPE_ACCELERATION_STRUCTURES,
+   SPIRV_TYPE_COUNT
 };
 
 enum SPIRV_Resource_Dim
@@ -85,78 +85,78 @@ enum SPIRV_Resource_Dim
 
 struct SPIRV_Resource
 {
-	// Spirv data type
-	SPIRV_Type SPIRV_code;
+   // Spirv data type
+   SPIRV_Type SPIRV_code;
 
-	// resource Type
-	SPIRV_Resource_Type type;
+   // resource Type
+   SPIRV_Resource_Type type;
 
-	// Texture dimension. Undefined if not a texture.
-	SPIRV_Resource_Dim dim;
+   // Texture dimension. Undefined if not a texture.
+   SPIRV_Resource_Dim dim;
 
-	// If the resouce was used in the shader
-	bool is_used;
+   // If the resouce was used in the shader
+   bool is_used;
 
-	// The resouce set if it has one
-	uint32_t set;
+   // The resouce set if it has one
+   uint32_t set;
 
-	// The resource binding location
-	uint32_t binding;
+   // The resource binding location
+   uint32_t binding;
 
-	// The size of the resouce. This will be the descriptor array size for textures
-	uint32_t size;
+   // The size of the resouce. This will be the descriptor array size for textures
+   uint32_t size;
 
-	// The declared name (OpName) of the resource.
-	// For Buffer blocks, the name actually reflects the externally
-	// visible Block name.
-	const char* name;
+   // The declared name (OpName) of the resource.
+   // For Buffer blocks, the name actually reflects the externally
+   // visible Block name.
+   const char* name;
 
-	// name size
-	uint32_t name_size;
+   // name size
+   uint32_t name_size;
 };
 
 struct SPIRV_Variable
 {
-	// Spirv data type
-	uint32_t SPIRV_type_id;
+   // Spirv data type
+   uint32_t SPIRV_type_id;
 
-	// parents SPIRV code
-	SPIRV_Type parent_SPIRV_code;
+   // parents SPIRV code
+   SPIRV_Type parent_SPIRV_code;
 
-	// parents resource index
-	uint32_t parent_index;
+   // parents resource index
+   uint32_t parent_index;
 
-	// If the data was used
-	bool is_used;
+   // If the data was used
+   bool is_used;
 
-	// The offset of the Variable.
-	uint32_t offset;
+   // The offset of the Variable.
+   uint32_t offset;
 
-	// The size of the Variable.
-	uint32_t size;
+   // The size of the Variable.
+   uint32_t size;
 
-	// Variable name
-	const char* name;
+   // Variable name
+   const char* name;
 
-	// name size
-	uint32_t name_size;
+   // name size
+   uint32_t name_size;
 };
 
 struct CrossCompiler
 {
-	// this points to the internal compiler class
-	void* pCompiler;
+   // this points to the internal compiler class
+   void* pCompiler;
 
-	// resources
-	SPIRV_Resource* pShaderResouces;
-	uint32_t ShaderResourceCount;
+   // resources
+   SPIRV_Resource* pShaderResouces;
+   uint32_t ShaderResourceCount;
 
-	// uniforms
-	SPIRV_Variable* pUniformVariables;
-	uint32_t UniformVariablesCount;
+   // uniforms
+   SPIRV_Variable* pUniformVariables;
+   uint32_t UniformVariablesCount;
 
-	char* pEntryPoint;
-	uint32_t EntryPointSize;
+   char* pEntryPoint;
+   uint32_t EntryPointSize;
 };
 
 void CreateCrossCompiler(const uint32_t* SpirvBinary, uint32_t BinarySize, CrossCompiler* outCompiler);
